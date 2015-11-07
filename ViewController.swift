6fbfc129a1e8a41b6042214a1f0832b6450bf168
linesmerrill <mlines@zagmail.gonzaga.edu>
2015-11-07 14:12:30 -0800

    ...

--- ViewController.swift
+++ ViewController.swift
@@ -43,9 +43,16 @@ class ViewController: UIViewController {
     @IBOutlet weak var zip: UITextField!
     @IBOutlet weak var phone: UITextField!
     @IBOutlet weak var email: UITextField!
+    //Bike Information Vars
+    @IBOutlet weak var brand: UITextField!
+    @IBOutlet weak var model: UITextField!
+    @IBOutlet weak var color: UITextField!
+    @IBOutlet weak var notes: UITextView!
     
-    @IBAction func OrderNext(sender: AnyObject) {
-        let NewParams = ["fname": fname.text!, "lname": lname.text!, "address":address.text!, "address2":address2.text!, "city":city.text!, "state":state.text!, "zip":zip.text!, "phone":phone.text!, "email":email.text!]
+    
+    @IBAction func SubmitButton(sender: AnyObject) {
+     
+        let NewParams = ["acton":"workOrder","fname": fname.text!, "lname": lname.text!, "address":address.text!, "address2":address2.text!, "city":city.text!, "state":state.text!, "zip":zip.text!, "phone":phone.text!, "email":email.text!, "brand":brand.text!, "model":model.text!, "color":color.text!, "notes":notes.text!]
         do {
             let opt = try HTTP.POST("http://107.170.219.218/Capstone/delegate.php", parameters: NewParams)
             opt.start {response in
