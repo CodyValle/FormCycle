9c11b7d96a96a80eb484da8645521f0f2569abe8
John Ragan <jpragan@johns-macbook-pro.local>
2015-11-18 14:00:34 -0800

    Added invoice view controller. fixed issue with submit/next buttons on customer info page

--- MyFormCycleHome/ViewController.swift
+++ MyFormCycleHome/ViewController.swift
@@ -11,7 +11,7 @@ import SwiftHTTP
 
 class ViewController: UIViewController
 {
-
+//******************** SIGN IN PAGE *****************************************
   /* This is the Login Page which will be implemented at a later date. 
    * Due to this, the section referred to as MARK: Login Page text and Server Request
    * will remain commented out until finally becoming implemented.
@@ -42,7 +42,9 @@ class ViewController: UIViewController
     }*/
   }
   
-  
+//**************************************************************************
+    
+//******************** NEW ORDER PAGE **************************************
   
   //MARK: New Order Page Text and Server Request
   
@@ -72,6 +74,11 @@ class ViewController: UIViewController
     static var myZip = ""
     static var myPhone = ""
     static var myEmail = ""
+    static var myBrand = ""
+    static var myModel = ""
+    static var myColor = ""
+    static var myNotes = ""
+    static var myTagNumber = ""
   }
   
     
@@ -80,18 +87,21 @@ class ViewController: UIViewController
    */
   //MARK: Variables and functions for Customer Information Page.
   @IBAction func submitCustInfo(sender: AnyObject)
-  {
-    newOrderTextFieldStruct.firstName = fname.text!
-    newOrderTextFieldStruct.lastName = lname.text!
-    newOrderTextFieldStruct.myAddress = address.text!
-    newOrderTextFieldStruct.myAddress2 = address2.text!
-    newOrderTextFieldStruct.myCity = city.text!
-    newOrderTextFieldStruct.myState = state.text!
+  {/*
+    
+    newOrderTextFieldStruct.firstName = fname.text! + " "
+    newOrderTextFieldStruct.lastName = lname.text! + " "
+    newOrderTextFieldStruct.myAddress = address.text! + " "
+    newOrderTextFieldStruct.myAddress2 = address2.text! + " "
+    newOrderTextFieldStruct.myCity = city.text! + " "
+    newOrderTextFieldStruct.myState = state.text! + " "
     newOrderTextFieldStruct.myZip = zip.text!
     newOrderTextFieldStruct.myPhone = phone.text!
-    newOrderTextFieldStruct.myEmail = email.text!
+    newOrderTextFieldStruct.myEmail = email.text!*/
   }
-  
+//***************************************************************************
+    
+//********************** BIKE INFO PAGE *************************************
   /* New Order Page: Bike Information. This section denotes the variables
    * as well as the functions that pretain to the Bike Information Page.
    */
@@ -104,8 +114,15 @@ class ViewController: UIViewController
   
   
   //Takes the user back to the Home Page
-  @IBAction func OrderCompleteSubmitButtonTop(sender: AnyObject)
+  @IBAction func OrderCompleteSubmitButtonTop(sender: UIBarButtonItem)
 	{
+      /*
+        newOrderTextFieldStruct.myBrand = brand.text! + " "
+        newOrderTextFieldStruct.myModel = model.text! + " "
+        newOrderTextFieldStruct.myColor = color.text!
+        newOrderTextFieldStruct.myNotes = notes.text!
+        newOrderTextFieldStruct.myTagNumber = tagNumber.text!
+        */
     let MyParams = ["action":"workOrder","fname":newOrderTextFieldStruct.firstName, "lname":newOrderTextFieldStruct.lastName, "address":newOrderTextFieldStruct.myAddress, "address2":newOrderTextFieldStruct.myAddress2, "city":newOrderTextFieldStruct.myCity, "state":newOrderTextFieldStruct.myState, "zip":newOrderTextFieldStruct.myZip, "phone":newOrderTextFieldStruct.myPhone, "email":newOrderTextFieldStruct.myEmail, "brand":brand.text!, "model":model.text!, "color":color.text!,
       "tagNum":tagNumber.text!,
       "notes":notes.text!]
@@ -140,19 +157,67 @@ class ViewController: UIViewController
     dismissViewControllerAnimated(true, completion: nil)
     
   }
-  
-  
-  //MARK: Pre-Defined functions
-  override func viewDidLoad() {
-    super.viewDidLoad()
-    // Do any additional setup after loading the view, typically from a nib.
-  }
-
-  override func didReceiveMemoryWarning() {
-    super.didReceiveMemoryWarning()
-    // Dispose of any resources that can be recreated.
-  }
-
-
+ //************************************************************************
+    
+//*********************** LOAD INVOICE PAGE *******************************
+    
+    @IBOutlet weak var invNotes: UILabel!
+    @IBOutlet weak var invTagNum: UILabel!
+    @IBOutlet weak var makeModelColor: UILabel!
+    @IBOutlet weak var invEmail: UILabel!
+    @IBOutlet weak var invPhone: UILabel!
+    @IBOutlet weak var CityStateZip: UILabel!
+    @IBOutlet weak var address3: UILabel!
+    @IBOutlet weak var address1: UILabel!
+    @IBOutlet weak var Name: UILabel!
+    @IBAction func loadInvoice(sender: AnyObject) {
+        Name.text = newOrderTextFieldStruct.firstName +  newOrderTextFieldStruct.lastName
+        address1.text = newOrderTextFieldStruct.myAddress
+        address3.text = newOrderTextFieldStruct.myAddress2
+        CityStateZip.text = newOrderTextFieldStruct.myCity + newOrderTextFieldStruct.myState + newOrderTextFieldStruct.myZip
+        invPhone.text = newOrderTextFieldStruct.myPhone
+        invEmail.text = newOrderTextFieldStruct.myEmail
+       makeModelColor.text = newOrderTextFieldStruct.myBrand + newOrderTextFieldStruct.myModel + "(" + newOrderTextFieldStruct.myColor + ")"
+        invTagNum.text = newOrderTextFieldStruct.myTagNumber
+        invNotes.text = newOrderTextFieldStruct.myNotes
+    }
+    
+//*************************************************************************
+    
+    
+    
+    //MARK: Pre-Defined functions
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject!) {
+        if segue.identifier == "moveToInvoice"{
+            newOrderTextFieldStruct.myBrand = brand.text! + " "
+            newOrderTextFieldStruct.myModel = model.text! + " "
+            newOrderTextFieldStruct.myColor = color.text!
+            newOrderTextFieldStruct.myNotes = notes.text!
+            newOrderTextFieldStruct.myTagNumber = tagNumber.text!
+            
+        }
+        if segue.identifier == "loadCustomerInfo"{
+            newOrderTextFieldStruct.firstName = fname.text! + " "
+            newOrderTextFieldStruct.lastName = lname.text! + " "
+            newOrderTextFieldStruct.myAddress = address.text! + " "
+            newOrderTextFieldStruct.myAddress2 = address2.text! + " "
+            newOrderTextFieldStruct.myCity = city.text! + " "
+            newOrderTextFieldStruct.myState = state.text! + " "
+            newOrderTextFieldStruct.myZip = zip.text!
+            newOrderTextFieldStruct.myPhone = phone.text!
+            newOrderTextFieldStruct.myEmail = email.text!
+            
+        }
+    }
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
 }
 
