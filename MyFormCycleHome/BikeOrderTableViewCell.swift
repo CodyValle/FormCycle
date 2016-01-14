30a7e947df8c724ce780c4b0ec3aca421c7ecb83
Cross <adam.cross60@gmail.com>
2016-01-14 12:06:21 -0800

    added the cell class

---
+++ MyFormCycleHome/BikeOrderTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  BikeOrderTableViewCell.swift
+//  FormCycle
+//
+//  Created by Cross, Adam B on 1/14/16.
+//  Copyright © 2016 Merrill Lines. All rights reserved.
+//
+
+import UIKit
+
+class BikeOrderTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
