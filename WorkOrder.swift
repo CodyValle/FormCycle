fb92be667acb411d475aee30eee70cc15791745b
Cross <adam.cross60@gmail.com>
2016-01-20 13:49:47 -0800

    Data model information added

--- WorkOrder.swift
+++ WorkOrder.swift
@@ -6,4 +6,29 @@
 //  Copyright © 2016 Merrill Lines. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+class WorkOrder {
+
+    //MARK: Properties
+    
+    let orderNumber: Int
+    let bikeType: String
+    let referenceNum: Int
+    let tune: String
+    
+    //MARK: Initialize properties
+    
+    init(orderNumber: Int, referenceNum: Int, tune: String, bikeType: String)
+    {
+        self.orderNumber = orderNumber
+        self.bikeType = bikeType
+        self.tune = tune
+        self.referencenNum = referenceNum
+        
+        if orderNumber.isEmpty || bikeType.isEmpty || tune.isEmpty || referenceNum.isEmpty
+        {
+            return nil
+        }
+    }
+}
