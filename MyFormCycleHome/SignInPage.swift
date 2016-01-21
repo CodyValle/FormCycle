e5ad81e0e5a636fcc680e6d469684c4a403b206d
Valle, Cody J <cvalle@pc106-mack.local>
2016-01-21 12:14:24 -0800

    ViewController split into multiple files.

---
+++ MyFormCycleHome/SignInPage.swift
@@ -0,0 +1,55 @@
+//
+//  SignInPage.swift
+//  FormCycle
+//
+//  Created by Valle, Cody J on 1/21/16.
+//  Copyright © 2016 Merrill Lines. All rights reserved.
+//
+
+import UIKit
+import SwiftHTTP
+import SwiftyJSON
+
+extension ViewController
+{
+    /*+------------------------------------ SIGN IN PAGE ------------------------------------+
+    | This is the Login Page which will be implemented at a later date.                    |
+    | Due to this, the section referred to as MARK: Login Page text and Server Request     |
+    | will remain commented out until finally becoming implemented.                        |
+    | MARK: Login Page text and Server Request                                             |
+    +--------------------------------------------------------------------------------------+*/
+    
+    
+    /* Function to allow the user a secret link to "This Bike Life's" Webpage.
+    *  We just wanted to have some fun and see if anyone finds this easter egg.
+    */
+    @IBAction func myLogoWebpageLink(sender: AnyObject) {
+        if let url = NSURL(string: "http://www.thisbikelife.com") {
+            UIApplication.sharedApplication().openURL(url)
+        }
+    }
+    
+    /* Will remain commented becuase it is not implemented yet */
+    @IBAction func MyButton(sender: AnyObject)
+    {/*
+        let params = ["action": USRTextField.text!, "pwd": PWDTextField.text!]
+        do
+        {
+        let opt = try HTTP.POST("http://107.170.219.218/Capstone/delegate.php", parameters: params)
+        opt.start
+        {
+        response in
+        if let error = response.error
+        {
+        print("got an error: \(error)")
+        return
+        }
+        print(response.text!)
+        }
+        }
+        catch let error
+        {
+        print("got an error creating the request: \(error)")
+        }*/
+    }
+}
--- MyFormCycleHome/ViewController.swift
+++ MyFormCycleHome/ViewController.swift
@@ -13,277 +13,39 @@ import SwiftyJSON
 
 class ViewController: UIViewController, UITextFieldDelegate, UITextViewDelegate
 {
-/*+------------------------------------ SIGN IN PAGE ------------------------------------+
-  | This is the Login Page which will be implemented at a later date.                    |
-  | Due to this, the section referred to as MARK: Login Page text and Server Request     |
-  | will remain commented out until finally becoming implemented.                        |
-	| MARK: Login Page text and Server Request                                             |
-  +--------------------------------------------------------------------------------------+*/
-  @IBOutlet weak var USRTextField: UITextField!
-  @IBOutlet weak var PWDTextField: UITextField!
-	
-	/* Function to allow the user a secret link to "This Bike Life's" Webpage.
-  *  We just wanted to have some fun and see if anyone finds this easter egg. 
-	*/
-	@IBAction func myLogoWebpageLink(sender: AnyObject) {
-		if let url = NSURL(string: "http://www.thisbikelife.com") {
-			UIApplication.sharedApplication().openURL(url)
-		}
-	}
-	
-	/* Will remain commented becuase it is not implemented yet */
-  @IBAction func MyButton(sender: AnyObject)
-  {/*
-    let params = ["action": USRTextField.text!, "pwd": PWDTextField.text!]
-    do
-    {
-    let opt = try HTTP.POST("http://107.170.219.218/Capstone/delegate.php", parameters: params)
-    opt.start
-    {
-      response in
-      if let error = response.error
-      {
-        print("got an error: \(error)")
-        return
-      }
-      print(response.text!)
-    }
-    }
-    catch let error
-    {
-    print("got an error creating the request: \(error)")
-    }*/
-  }
-  
-/*+------------------------------------ NEW ORDER PAGE ------------------------------------+
-  | New Order Page is the hub for a new order. This will collect all the customer info     |
-  | and save those values to a global struct. This struct allows us to access the saved    |
-  | values from this page on any other page in the app.                                    |
-  | MARK: New Order Page Text and Server Request                                           |
-  +----------------------------------------------------------------------------------------+*/
-  /* List all Text Fields imported from New Order Page */
-  @IBOutlet weak var fname: UITextField!
-  @IBOutlet weak var lname: UITextField!
-  @IBOutlet weak var address: UITextField!
-  @IBOutlet weak var address2: UITextField!
-  @IBOutlet weak var city: UITextField!
-  @IBOutlet weak var state: UITextField!
-  @IBOutlet weak var zip: UITextField!
-  @IBOutlet weak var phone: UITextField!
-  @IBOutlet weak var email: UITextField!
-  
-  /* newOrderTextFieldStruct is a struct made to store text in Text Fields for
-  *  use when submitting the order on the Bike Info Page.
-  *  Initially sets all values in struct to the empty
-  *  string. */
-  struct newOrderTextFieldStruct
-  {
-    static var firstName = ""
-    static var lastName = ""
-    static var myAddress = ""
-    static var myAddress2 = ""
-    static var myCity = ""
-    static var myState = ""
-    static var myZip = ""
-    static var myPhone = ""
-    static var myEmail = ""
-    static var myBrand = ""
-    static var myModel = ""
-    static var myColor = ""
-    static var myNotes = ""
-    static var myTagNumber = ""
-		static var neworderpage = false
-		static var bikeInfoPage = false
-		static var invoicePage = false
-  }
-  
-	/* Sends the user back to the Home Page if currently on the Customer Information page. */
-	@IBAction func backToHomePageBtn(sender: AnyObject)
-	{
-		newOrderTextFieldStruct.neworderpage = false /* sets current view to false */
-		dismissViewControllerAnimated(true, completion: nil) /* dismisses the view controller */
-	}
-  /* New Order Page: Customer Information. This section denotes the variables
-   * as well as the functions that pretain to the Customer Information Page.
-   */
-  //MARK: Variables and functions for Customer Information Page.
-  @IBAction func submitCustInfo(sender: AnyObject)
-  {
-    newOrderTextFieldStruct.firstName = fname.text!
-    newOrderTextFieldStruct.lastName = lname.text!
-    newOrderTextFieldStruct.myAddress = address.text!
-    newOrderTextFieldStruct.myAddress2 = address2.text!
-    newOrderTextFieldStruct.myCity = city.text!
-    newOrderTextFieldStruct.myState = state.text!
-    newOrderTextFieldStruct.myZip = zip.text!
-    newOrderTextFieldStruct.myPhone = phone.text!
-    newOrderTextFieldStruct.myEmail = email.text!
-  }
-	
-    @IBAction func RetrieveCustomerInfo(sender: AnyObject)
-    {
-        /* Submits the server request */
-        var MyParams = ["action":"custSearch"]
-        
-        // Append possible search data to the parameters. Note: MyParams is changed to a var, instead of a let.
-        /*if (DEBUG) {
-            MyParams["DEBUG"] = "true"
-        }*/
-        if (fname.text != nil) {
-            MyParams["fname"] = fname.text!
-        }
-        if (lname.text != nil) {
-            MyParams["lname"] = lname.text!
-        }
-        if (address.text != nil) {
-            MyParams["address"] = address.text!
-        }
-        if (address2.text != nil) {
-            MyParams["address2"] = address2.text!
-        }
-        if (city.text != nil) {
-            MyParams["city"] = city.text!
-        }
-        if (state.text != nil) {
-            MyParams["state"] = state.text!
-        }
-        if (zip.text != nil) {
-            MyParams["zip"] = zip.text!
-        }
-        if (phone.text != nil) {
-            MyParams["phone"] = phone.text!
-        }
-        if (email.text != nil) {
-            MyParams["email"] = email.text!
-        }
+    /* List all Text Fields imported from Sign In Page */
+    @IBOutlet weak var USRTextField: UITextField!
+    @IBOutlet weak var PWDTextField: UITextField!
+    
+    /* List all Text Fields imported from New Order Page */
+    @IBOutlet weak var fname: UITextField!
+    @IBOutlet weak var lname: UITextField!
+    @IBOutlet weak var address: UITextField!
+    @IBOutlet weak var address2: UITextField!
+    @IBOutlet weak var city: UITextField!
+    @IBOutlet weak var state: UITextField!
+    @IBOutlet weak var zip: UITextField!
+    @IBOutlet weak var phone: UITextField!
+    @IBOutlet weak var email: UITextField!
+    
+    /* List all Text Fields imported from Bike Information Page */
+    @IBOutlet weak var brand: UITextField!
+    @IBOutlet weak var model: UITextField!
+    @IBOutlet weak var color: UITextField!
+    @IBOutlet weak var notes: UITextView!
+    @IBOutlet weak var tagNumber: UITextField!
+    
+    /* List all Text Fields imported from Load Invoice Page */
+    @IBOutlet weak var invNotes: UILabel!
+    @IBOutlet weak var invTagNum: UILabel!
+    @IBOutlet weak var makeModelColor: UILabel!
+    @IBOutlet weak var invEmail: UILabel!
+    @IBOutlet weak var invPhone: UILabel!
+    @IBOutlet weak var CityStateZip: UILabel!
+    @IBOutlet weak var address3: UILabel!
+    @IBOutlet weak var address1: UILabel!
+    @IBOutlet weak var Name: UILabel!
 
-        do
-        {
-            /* tries to submit to server */
-            let opt = try HTTP.POST("http://107.170.219.218/Capstone/delegate.php", parameters: MyParams)
-            opt.start
-            {
-                response in
-                if let error = response.error
-                {
-                    print("got an error: \(error)") /* if error, prints the error code saved on server */
-                    return
-                }
-                if (response.text != nil)
-                {
-                    if let datafromstring = response.text!.dataUsingEncoding(NSUTF8StringEncoding, allowLossyConversion: false)
-                    {
-                        let json = JSON(data: datafromstring)
-                        
-                        // Some helpful debug data for use when needing to place in table.
-                        print("There are \(json.count) rows matching the supplied data.")
-                        if (json.count > 0)
-                        {
-                            // UI updates should not occur on a non-main thread. So call it on the main thread.
-                            dispatch_async(dispatch_get_main_queue())
-                            {
-                                // Write all the data to the text fields. We first check to make sure the textfield does not already contain data and that the wanted value exists in the JSON string.
-                                // The [0] is the index of the row that we want to auto fill with.
-                                if (json[0]["fname"].isExists()) {
-                                    self.fname.text = json[0]["fname"].string
-                                }
-                                if (json[0]["lname"].isExists()) {
-                                    self.lname.text = json[0]["lname"].string
-                                }
-                                if (json[0]["address"].isExists()) {
-                                    self.address.text = json[0]["address"].string
-                                }
-                                if (json[0]["address2"].isExists()) {
-                                    self.address2.text = json[0]["address2"].string
-                                }
-                                if (json[0]["city"].isExists()) {
-                                    self.city.text = json[0]["city"].string
-                                }
-                                if (json[0]["state"].isExists()) {
-                                    self.state.text = json[0]["state"].string
-                                }
-                                if (json[0]["zip"].isExists()) {
-                                    self.zip.text = json[0]["zip"].string
-                                }
-                                if (json[0]["phone"].isExists()) {
-                                    self.phone.text = json[0]["phone"].string
-                                }
-                                if (json[0]["email"].isExists()) {
-                                    self.email.text = json[0]["email"].string
-                                }
-                            }
-                        }
-                    }
-                }
-            }
-        }
-        catch let error
-        {
-            print("got an error creating the request: \(error)")
-        }
-    }
-
-/*+------------------------------------ BIKE INFO PAGE ------------------------------------+
-  | Bike info page collects all revelant info pertaining to the bike itself. Here we       |
-  | collect the Model, Brand, Color, Tag Number, and Notes about the bike.                 |
-  | MARK: Variables and functions for Bike Information Page.                               |
-  +----------------------------------------------------------------------------------------+*/
-  /* New Order Page: Bike Information. This section denotes the variables
-   * as well as the functions that pretain to the Bike Information Page.
-   */
-  @IBOutlet weak var brand: UITextField!
-  @IBOutlet weak var model: UITextField!
-  @IBOutlet weak var color: UITextField!
-  @IBOutlet weak var notes: UITextView!
-  @IBOutlet weak var tagNumber: UITextField!
-	
-  /* Made to set the text pre-populated in the text
-   * boxes when the user pressed back on the
-   * RepairInfo Page.
-   */
-  @IBAction func backToCustInfo(sender: AnyObject)
-	{
-    newOrderTextFieldStruct.bikeInfoPage = false /* sets current view to false */
-    dismissViewControllerAnimated(true, completion: nil) /* dismisses the current view */
-		newOrderTextFieldStruct.neworderpage = true /* sets the new order page view to true since
-																									 user just pressed the "back" button */
-  }
-
-/*+-------------------------------- LOAD INVOICE PAGE ------------------------------------+
-  | Load invoice page displays all recorded information collected through the customer    |
-  | information page, and bike information page. This allows the tech to see if all info  |
-  | was correctly collected.                                                              |
-  | MARK: Variables and functions for Load Invoice Page.                                  |
-  +---------------------------------------------------------------------------------------+*/
-	/* Load Invoice Page. This section denotes the variables as well as the functions that
-   * pertain to the Load Invoice Page. The goal for this page is to have the information
-   * automatically load when the view is loaded to the page.
-   * !!! STILL IN PROGRESS !!!
-   */
-	
-	
-	@IBOutlet weak var invNotes: UILabel!
-	@IBOutlet weak var invTagNum: UILabel!
-	@IBOutlet weak var makeModelColor: UILabel!
-	@IBOutlet weak var invEmail: UILabel!
-	@IBOutlet weak var invPhone: UILabel!
-	@IBOutlet weak var CityStateZip: UILabel!
-	@IBOutlet weak var address3: UILabel!
-	@IBOutlet weak var address1: UILabel!
-	@IBOutlet weak var Name: UILabel!
-	/* Loads all values from struct into text fields on this page */
-//	@IBAction func loadInvoice(sender: AnyObject)
-//	{
-//		Name.text = newOrderTextFieldStruct.firstName +  newOrderTextFieldStruct.lastName
-//		address1.text = newOrderTextFieldStruct.myAddress
-//		address3.text = newOrderTextFieldStruct.myAddress2
-//		CityStateZip.text = newOrderTextFieldStruct.myCity + newOrderTextFieldStruct.myState + newOrderTextFieldStruct.myZip
-//		invPhone.text = newOrderTextFieldStruct.myPhone
-//		invEmail.text = newOrderTextFieldStruct.myEmail
-//		makeModelColor.text = newOrderTextFieldStruct.myBrand + newOrderTextFieldStruct.myModel + "(" + newOrderTextFieldStruct.myColor + ")"
-//		invTagNum.text = newOrderTextFieldStruct.myTagNumber
-//		invNotes.text = newOrderTextFieldStruct.myNotes
-//	}
 	
 /*+------------------------------- viewDidLoad() --------------------------------------+
 	| viewDidLoad() is a function that is overwritten here. Here we modify the view to   |
