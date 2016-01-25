3c1db0ac2a69f3ef0acf0227083620ff31057b42
Cross <adam.cross60@gmail.com>
2016-01-25 15:58:17 -0800

    began process of pulling data from database. Some issues, but will be resolved

--- MyFormCycleHome/WorkOrderTableViewController.swift
+++ MyFormCycleHome/WorkOrderTableViewController.swift
@@ -17,7 +17,64 @@ class WorkOrderTableViewController: UITableViewController {
     
     func loadSampleData()
     {
-        let order1 = WorkOrder(orderNumber: "12", orderID: "1", tune: "A Thing.", bikeType:"Schwin")
+        /* Submits the server request */
+        var MyParams = ["action":"workSearch"]
+        
+        // Append possible search data to the parameters. Note: MyParams is changed to a var, instead of a let.
+        MyParams["open"] = "Y"
+        MyParams["lname"] = "Bowers"
+        do
+        {
+            /* tries to submit to server */
+            let opt = try HTTP.POST("http://107.170.219.218/Capstone/delegate.php", parameters: MyParams)
+            opt.start
+                {
+                    response in
+                    if let error = response.error
+                    {
+                        print("got an error: \(error)") /* if error, prints the error code saved on server */
+                        return
+                    }
+                    if (response.text != nil)
+                    {
+                        print(response.text!)
+                        if let datafromstring = response.text!.dataUsingEncoding(NSUTF8StringEncoding, allowLossyConversion: false)
+                        {
+                            
+                            let json = JSON(data: datafromstring)
+                            if (json["success"])
+                            {
+                                
+                                // Probably needs more error checks.
+                                let retString = json["return"].string!.dataUsingEncoding(NSUTF8StringEncoding, allowLossyConversion: false)
+                                let orders = JSON(data: retString!)
+                                print(orders.count)
+                                if (orders.count > 0) // Fill the form
+                                {
+                                    for var i = 0; i < orders.count; i++
+                                    {
+                                        
+                                        self.workOrders += [WorkOrder(orderNumber: orders[i]["workid"].string!, orderID:orders[i]["workid"].string!, tune: "Bronze", bikeType:orders[i]["brand"].string!)]
+                                    }
+                                }
+                                //else you are done- TO DO LATER
+                            }
+                            
+                            // Some helpful debug data for use when needing to place in table.
+                            print("There are \(json.count) rows matching the supplied data.")
+                            print(json);
+                        }
+                    }
+            }
+        }
+        catch let error
+        {
+            print("got an error creating the request: \(error)")
+        }
+        
+        print(self.workOrders)
+        
+        /*let order1 = WorkOrder(orderNumber: "12", orderID: "1", tune: "A Thing.", bikeType:"Schwin")
         let order2 = WorkOrder(orderNumber: "13", orderID: "2", tune: "A Thing.", bikeType:"Schwin")
         let order3 = WorkOrder(orderNumber: "14", orderID: "3", tune: "A Thing.", bikeType:"Schwin")
         let order4 = WorkOrder(orderNumber: "15", orderID: "4", tune: "A Thing.", bikeType:"Schwin")
@@ -26,7 +83,8 @@ class WorkOrderTableViewController: UITableViewController {
         let order7 = WorkOrder(orderNumber: "18", orderID: "7", tune: "A Thing.", bikeType:"Schwin")
         let order8 = WorkOrder(orderNumber: "19", orderID: "8", tune: "A Thing.", bikeType:"Schwin")
         
-        workOrders += [order1,order2,order3,order4,order5,order6,order7,order8]
+        workOrders += [order1,order2,order3,order4,order5,order6,order7,order8]*/
+      
     }
     
     
