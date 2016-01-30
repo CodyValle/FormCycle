f561b56fa4ad78d9c2e995be701b9228e177a752
Valle <cvalle@PC106-macK.local>
2016-01-30 12:27:12 -0800

    PHP for commenting

---
+++ Capstone/login.php
@@ -0,0 +1,26 @@
+<?php
+
+include_once 'MySQLCommand.php';
+
+class Login
+{
+	public static function run(&$clean)
+	{
+		// Create the command
+		$sel = new MySQLSelectCommand('LogInData');
+		$sel->addColumn('username');
+		$sel->addParameter('username', $clean['logid']);
+		$sel->addParameter('pin', $clean['pwd']);
+		
+		$out = $GLOBALS['con']->query($sel->getSQL());
+		
+		// Get return value
+		$ret = $out->num_rows > 0;
+		
+		// Close SELECT results
+		$out->close();
+		
+		return $ret;
+	}
+}
+?>
