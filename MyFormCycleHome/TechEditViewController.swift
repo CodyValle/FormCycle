9a3d840bef248cadb6673fc61d461f9a070427d6
Valle, Cody J <cvalle@pc106-mack.local>
2016-02-23 11:34:19 -0800

    Saving edited Work Order notes on the Back button and Close Order button.

--- MyFormCycleHome/TechEditViewController.swift
+++ MyFormCycleHome/TechEditViewController.swift
@@ -42,49 +42,49 @@ class TechEditViewController: UIViewController, UITextFieldDelegate, UITextViewD
             let opt = try HTTP.POST("http://107.170.219.218/Capstone/delegate.php", parameters: MyParams)
             
             opt.start
+            {
+                response in
+                if let error = response.error
                 {
-                    response in
-                    if let error = response.error
+                    print("got an error: \(error)") /* if error, prints the error code saved on server */
+                    return
+                }
+                if (response.text != nil)
+                {
+                    if let datafromstring = response.text!.dataUsingEncoding(NSUTF8StringEncoding, allowLossyConversion: false)
                     {
-                        print("got an error: \(error)") /* if error, prints the error code saved on server */
-                        return
-                    }
-                    if (response.text != nil)
-                    {
-                        if let datafromstring = response.text!.dataUsingEncoding(NSUTF8StringEncoding, allowLossyConversion: false)
+                        let json = JSON(data: datafromstring)
+                        if (json["success"])
                         {
-                            let json = JSON(data: datafromstring)
-                            if (json["success"])
+                            // Probably needs more error checks.
+                            let retString = json["return"].string!.dataUsingEncoding(NSUTF8StringEncoding, allowLossyConversion: false)
+                            let order = JSON(data: retString!)
+                            if (order.count > 0) // Fill the form
                             {
-                                // Probably needs more error checks.
-                                let retString = json["return"].string!.dataUsingEncoding(NSUTF8StringEncoding, allowLossyConversion: false)
-                                let order = JSON(data: retString!)
-                                if (order.count > 0) // Fill the form
+                                self.userID.text = order[0]["userID"].string!
+                                self.fullName.text = order[0]["fname"].string! + " " + order[0]["lname"].string!
+                                self.address.text = order[0]["address"].string!
+                                if(order[0]["address2"] != nil)
                                 {
-	                                  self.userID.text = order[0]["userID"].string!
-                                    self.fullName.text = order[0]["fname"].string! + " " + order[0]["lname"].string!
-                                    self.address.text = order[0]["address"].string!
-                                    if(order[0]["address2"] != nil)
-                                    {
-                                        self.address.text =  self.address.text! + " " + order[0]["address2"].string!
-                                    }
-                                    self.cityNState.text = order[0]["city"].string! + ", " + order[0]["state"].string! + ", " + order[0]["zip"].string!
-                                    self.bikeInfo.text = order[0]["brand"].string! + " " + order[0]["model"].string! + ", " + order[0]["color"].string!
-                                    self.tune.text = order[0]["tune"].string!
-                                    self.tagNum.text = order[0]["tagnum"].string!
-                                    if(order[0]["notes"] != nil)
-                                    {
-                                      self.storedNote = order[0]["notes"].string!
-                                    }
-                                  //self.notes.text = "Hello"
-                                    
+                                    self.address.text =  self.address.text! + " " + order[0]["address2"].string!
                                 }
-                                //else you are done- TO DO LATER
+                                self.cityNState.text = order[0]["city"].string! + ", " + order[0]["state"].string! + ", " + order[0]["zip"].string!
+                                self.bikeInfo.text = order[0]["brand"].string! + " " + order[0]["model"].string! + ", " + order[0]["color"].string!
+                                self.tune.text = order[0]["tune"].string!
+                                self.tagNum.text = order[0]["tagnum"].string!
+                                if(order[0]["notes"] != nil)
+                                {
+                                  self.storedNote = order[0]["notes"].string!
+                                }
+                              //self.notes.text = "Hello"
+                                
                             }
+                            //else you are done- TO DO LATER
                         }
                     }
-                    // Some helpful debug data for use when needing to place in table.
-                    isDoneLoading = true
+                }
+                // Some helpful debug data for use when needing to place in table.
+                isDoneLoading = true
             }
         }
         catch let error
@@ -101,6 +101,8 @@ class TechEditViewController: UIViewController, UITextFieldDelegate, UITextViewD
         var MyParams = ["action":"workUpdate"]
         MyParams["workid"] = workid
         MyParams["open"] = "N"
+	      MyParams["notes"] = notes.text
+      
         do
         {
             /* tries to submit to server */
@@ -139,11 +141,54 @@ class TechEditViewController: UIViewController, UITextFieldDelegate, UITextViewD
         } // catch
     }
 
-    
+    @IBAction func BackToHomePage(sender: AnyObject)
+    {
+      /* Updates the notes */
+      var MyParams = ["action":"workUpdate"]
+      MyParams["workid"] = workid
+      MyParams["notes"] = notes.text
+      do
+      {
+        /* tries to submit to server */
+        let opt = try HTTP.POST("http://107.170.219.218/Capstone/delegate.php", parameters: MyParams)
+        opt.start
+          {
+            response in
+            if let error = response.error
+            {
+              print("got an error: \(error)") /* if error, prints the error code saved on server */
+              return
+            }
+
+            // No errors
+            if (response.text != nil)
+            {
+              if let datafromstring = response.text!.dataUsingEncoding(NSUTF8StringEncoding, allowLossyConversion: false)
+              {
+                let json = JSON(data: datafromstring)
+
+                if (json["success"])
+                {
+                  //print("Successfully closed order")
+                }
+                else
+                {
+                  // print("Failed to close order")
+                }
+              } //if let datastring = ...
+            } // if (response.text != null)
+        } // opt.start
+      } // do
+      catch let error
+      {
+        print("got an error creating the request: \(error)")
+      } // catch
+    }
+
     @IBAction func closeOrder(sender: AnyObject)
     {
         let refreshAlert = UIAlertController(title: "Confirmation", message: "Order will be closed and can no longer be edited.", preferredStyle: UIAlertControllerStyle.Alert)
-        
+
         refreshAlert.addAction(UIAlertAction(title: "Ok", style: .Default, handler: { (action: UIAlertAction!) in
             self.submitServerRequest()
             self.performSegueWithIdentifier("closedOrderSegue", sender: self)
