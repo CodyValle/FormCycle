8d2ceb1058a87e9e08bd8aaab5e8c0f287c0b184
Merrill Lines <merrilliscool@hotmail.com>
2016-03-03 16:08:02 -0800

    Search files added.

---
+++ MyFormCycleHome/Candy.swift
@@ -0,0 +1,15 @@
+//
+//  Candy.swift
+//  FormCycle
+//
+//  Created by Merrill Lines on 2/29/16.
+//  Copyright © 2016 Merrill Lines. All rights reserved.
+//
+
+import Foundation
+
+struct Candy {
+    let category : String
+    let name : String
+    let address: String
+}
