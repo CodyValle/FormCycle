441b18172386c28d10caed16bc8d19919e8ae280
Merrill Lines <merrilliscool@hotmail.com>
2016-03-26 16:04:43 -0700

    Added new button for Edit Tune on Admin Page. Also created new view controllers for Edit Users, and Edit Tunes.

---
+++ MyFormCycleHome/EditRemoveUserViewController.swift
@@ -0,0 +1,15 @@
+//
+//  EditRemoveUser.swift
+//  FormCycle
+//
+//  Created by Merrill Lines on 3/26/16.
+//  Copyright © 2016 Merrill Lines. All rights reserved.
+//
+
+import UIKit
+
+class EditRemoveUserViewController: UIViewController
+{
+    
+    
+}
---
+++ MyFormCycleHome/EditTuneViewController.swift
@@ -0,0 +1,15 @@
+//
+//  EditTuneViewController.swift
+//  FormCycle
+//
+//  Created by Merrill Lines on 3/26/16.
+//  Copyright © 2016 Merrill Lines. All rights reserved.
+//
+
+import UIKit
+
+class EditTuneViewController: UIViewController
+{
+    
+    
+}
