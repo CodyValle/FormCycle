4a6998e4697ced4bfe96c0a080db02ab390e7223
Merrill Lines <merrilliscool@hotmail.com>
2016-03-26 17:10:03 -0700

    Added pages for the Edit/Remove Users Page. Currently working on the tables.

---
+++ MyFormCycleHome/EditUsersTableViewController.swift
@@ -0,0 +1,106 @@
+
+//
+//  WorkOrderTableViewController.swift
+//  FormCycle
+//
+//  Created by Lines, Merrill on 3/26/2016
+//  Copyright © 2016 FormCycle Developers. All rights reserved.
+//
+
+import UIKit
+import SwiftHTTP
+import SwiftyJSON
+
+class EditUsersTableViewController: UITableViewController
+{
+    
+    // MARK: Properties
+    var workOrders = [WorkOrder]()
+    
+    override func viewDidLoad()
+    {
+        super.viewDidLoad()
+        
+        //Load data
+        workOrders.removeAll()
+        
+        /* Submits the server request */
+        var MyParams = ["action":"workSearch"]
+        
+        // Append possible search data to the parameters. Note: MyParams is changed to a var, instead of a let.
+        MyParams["open"] = "Y"
+        
+        ServerCom.send(MyParams, f: {(succ: Bool, retjson: JSON) in
+            if (succ) {
+                if (retjson.count > 0) {
+                    for var i = 0; i < retjson.count; i++ {
+                        self.workOrders.append(WorkOrder(tagNumber: retjson[i]["tagnum"].string!,
+                            orderID:   retjson[i]["workid"].string!,
+                            tune:      retjson[i]["tune"].string!,
+                            bikeType:  retjson[i]["brand"].string!,
+                            model:     retjson[i]["model"].string!,
+                            lname:     Crypto.decrypt(retjson[i]["lname"].string!)))
+                        
+                        dispatch_async(dispatch_get_main_queue()) {
+                            self.tableView.reloadData()
+                        }
+                    }
+                }
+                //else you are done- TO DO LATER
+                return true
+            }
+            return false
+        })
+    }
+    
+    override func didReceiveMemoryWarning()
+    {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    // MARK: - Table view data source
+    override func numberOfSectionsInTableView(tableView: UITableView) -> Int
+    {
+        return 1
+    }
+    
+    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int
+    {
+        return workOrders.count
+    }
+    
+    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell
+    {
+        let cellIdentifier = "UsersTableViewCell"
+        
+        //Set the cell as the BikeOrderTableViewCell class, using the WorkOrder data model
+        let cell = tableView.dequeueReusableCellWithIdentifier(cellIdentifier, forIndexPath: indexPath) as! UsersTableViewCell
+        let order = workOrders[indexPath.row]
+        
+        //Setting cell attributes to those in our array
+        cell.fname.text = order.bikeType
+        cell.lname.text = order.tune
+        cell.username.text = order.tagNumber
+        
+        
+        //cell.backgroundColor = indexPath.row % 2 == 0 ? UIColor(white: 1.0, alpha: 1.0) : UIColor(white: 0.7, alpha: 1.0)
+        return cell
+    }
+    
+    // MARK: - Navigation
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    let techSegueIndetifier = "TechEditSegue"
+    
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?)
+    {
+        if segue.identifier == techSegueIndetifier {
+            if let destination = segue.destinationViewController as? TechEditViewController {
+                if let orderIndex = tableView.indexPathForSelectedRow?.row {
+                    destination.workidPassed = workOrders[orderIndex].orderID
+                }
+            }
+        }
+    }
+    
+}
---
+++ MyFormCycleHome/UsersTableViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  UsersTableViewCell.swift
+//  FormCycle
+//
+//  Created by Merrill Lines on 3/26/16.
+//  Copyright © 2016 Merrill Lines. All rights reserved.
+//
+
+import UIKit
+
+class UsersTableViewCell: UITableViewCell
+{
+    
+    @IBOutlet weak var fname: UILabel!
+    @IBOutlet weak var lname: UILabel!
+    @IBOutlet weak var username: UILabel!
+}
