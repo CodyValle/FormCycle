0ed072ef69faf144d364a6bd3966d89282b73065
Merrill Lines <linesmerrill@gmail.com>
2016-03-29 13:07:18 -0700

    Allowed clickable elements in table.

---
+++ MyFormCycleHome/EditUserViewController.swift
@@ -0,0 +1,20 @@
+//
+//  EditUserViewController.swift
+//  FormCycle
+//
+//  Created by Merrill Lines on 3/29/16.
+//  Copyright © 2016 Merrill Lines. All rights reserved.
+//
+
+import UIKit
+
+class EditUserViewController: UIViewController
+{
+    @IBAction func backToEditRemoveTable(sender: AnyObject) {
+        dismissViewControllerAnimated(true, completion: nil) /* dismisses the current view */
+        
+    }
+    
+    
+    
+}
