b0e1c332c2e207453608bb70fbe16675e5eaad64
John Ragan <jpragan16@gmail.com>
2016-04-09 16:52:48 -0700

    add services update

---
+++ MyFormCycleHome/AddServices.swift
@@ -0,0 +1,28 @@
+//
+//  AddServices.swift
+//  FormCycle
+//
+//  Created by Merrill Lines on 4/9/16.
+//  Copyright © 2016 Merrill Lines. All rights reserved.
+//
+
+import UIKit
+
+class AddServices
+{
+    //MARK: Properties
+    
+    let title: String
+    let detail: String
+    //let password: String
+    
+    
+    //MARK: Initialize properties
+    init(title: String, detail: String)
+    {
+        self.title = title
+        self.detail = detail
+        //self.password = password
+        
+    }
+}
